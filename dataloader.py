dc92fac0853e47ab138bcfbbac97d27097e66064
veryunstablerice <veryunstablerice@proton.me>
2024-08-05 21:25:22 +0300

    dataloader is fully here

--- dataloader.py
+++ dataloader.py
@@ -1,13 +1,16 @@
+import random
+import torch
 import torchaudio
 from torch.utils.data import Dataset, DataLoader
 import os
 
 
 class SongDataset(Dataset):
-    def __init__(self, training_dir):
+    def __init__(self, training_dir, segment_lenght=48000):
         super().__init__()
         self.filenames = [f for f in os.listdir(training_dir) if f.endswith('.mp3')]
         self.training_dir = training_dir
+        self.segment_lenght = segment_lenght
 
     def __len__(self):
         return len(self.filenames)
@@ -18,15 +21,18 @@ class SongDataset(Dataset):
 
         if sample_rate != 16000:
             waveform = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=16000)(waveform)
+        if waveform.shape[0] > 1:
+            waveform = torch.mean(waveform, dim=0, keepdim=True)
+
+        if waveform.shape[1] > self.segment_lenght:
+            max = waveform.shape[1] - self.segment_lenght
+            start = random.randint(0, max)
+            waveform = waveform[:, start:start+self.segment_lenght]
+        else:
+            waveform = torch.nn.functional.pad(waveform, (0, self.segment_lenght-waveform.shape[1]))
 
         return waveform
 
 
-root_dir = './training_data'
-dataset = SongDataset(root_dir)
-dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
-
-for i, data in enumerate(dataloader):
-    print(data.shape)
-    if i == 5:  # Limit the output for demonstration purposes
-        break
+def save_audio(tensor, output_dir, file_name):
+    torchaudio.save(os.path.join(output_dir, file_name), tensor, 16000)
